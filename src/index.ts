850c59f8a1fb231ab4d4b1d084ae2f8567a31787
zangchuqiang <zangchuqiang@chengfayun.com>
2020-10-30 23:29:34 +0800

    add commit-lint

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { command, parse, version } from "commander";
+
+// @ts-ignore
+import packageJson from "../package.json";
+
+// commands
+import { commitLint } from "./commands/commit-lint";
+import { clean } from "./commands/clean";
+
+version(`@gleaning/check ${packageJson.version}`);
+process.env.GLEANING_VERSION = packageJson.version;
+
+command("clean").description("Clean all dist files").action(clean);
+
+command("commit-lint").description("Lint commit message").action(commitLint);
+
+parse();
